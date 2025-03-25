74442ecb5c7d708d5c86e0087bc2364f0d4a7c8c
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-03-25 10:33:56 +0700

    menggunakan perulangan for untuk menyimpan data pada Array

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -26,4 +26,11 @@ void input()
     cout << "Masukan Elemen Array" << endl;
     cout << "===================" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+    
+
 }
