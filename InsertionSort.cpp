e51307d2c887d681bb61d32d1b5de27069042377
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-03-25 10:48:34 +0700

    Membuat perulangan for looping dengan j dimulai dari 0 hingga n-1

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -62,4 +62,20 @@ void display()
     cout << "=====================" << endl;
     cout << "Elemen Array yang telah tersusun" << endl;
     cout << "=====================" << endl;
+
+    for (int j = 0; j < n; j++)
+    {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    input();
+    InsertionSort();
+    display();
+    system("pause");
+    return 0;
 }
